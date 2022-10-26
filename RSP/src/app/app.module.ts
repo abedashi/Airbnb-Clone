4a6ff79424ad3db80a16f3c448ec1003eaf8fcc8
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-10-27 01:35:34 +0300

    add import module and script tag

--- RSP/src/app/app.module.ts
+++ RSP/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListingComponent } from './listing/listing.component';
+import { MapsComponent } from './maps/maps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListingComponent
+    ListingComponent,
+    MapsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
