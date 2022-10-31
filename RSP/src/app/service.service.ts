68bd0d84e08131700bd0d34ac3c56938fb83bd61
habibashi <habibashi187@gmail.com>
2022-10-31 18:14:32 +0200

    listing array updates

--- RSP/src/app/service.service.ts
+++ RSP/src/app/service.service.ts
@@ -10,35 +10,49 @@ export class ServiceService {
 
   listing: List[] = [
     new List(
-      'Mar Mikhael', '195 Kilometers away', new Date(), 171, 4.1,
+      'Mar Mikhael', new Date(), 171, 4.1, 
       'https://images.pexels.com/photos/13415959/pexels-photo-13415959.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-      { lat: 33.9015, lng: 35.5237 }
-    ),
-    new List(
-      'Central Military Club', '185 Kilometers away', new Date(), 250, 3.9,
-      'https://images.pexels.com/photos/6091193/pexels-photo-6091193.jpeg?auto=compress&cs=tinysrgb&w=800',
-      { lat: 33.897349558489125, lng: 35.47030448913574 }
-    ),
-    new List(
-      'Lebanon', '185 Kilometers away', new Date(), 100, 5.0,
-      'https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
-      { lat: 33.8966, lng: 35.4823 }
-    ),
-    new List(
-      'Beirut Central District', '185 Kilometers away', new Date(), 230, 4.5,
+      { lat: 33.9015, lng: 35.5237 },
+      ['https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
       'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
-      { lat: 33.9055, lng: 35.5069 }
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600'
+      ], 2, 3, 1, 2
     ),
     new List(
-      'Mar Elias', '185 Kilometers away', new Date(), 99, 4.2,
-      'https://images.pexels.com/photos/1382734/pexels-photo-1382734.jpeg?auto=compress&cs=tinysrgb&w=800',
-      { lat: 33.8772, lng: 35.4886 }
+      'Central Military Club', new Date(), 250, 3.9,
+      'https://images.pexels.com/photos/6091193/pexels-photo-6091193.jpeg?auto=compress&cs=tinysrgb&w=800',
+      { lat: 33.897349558489125, lng: 35.47030448913574 },
+      ['https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600'
+      ], 2, 3, 1, 2
     ),
     new List(
-      'ABC Verdun', '155 Kilometers away', new Date(), 280, 4.8,
-      'https://images.pexels.com/photos/12993543/pexels-photo-12993543.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-      { lat: 33.8843, lng: 35.4846 }
+      'Lebanon', new Date(), 100, 5.0,
+      'https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
+      { lat: 33.8966, lng: 35.4823 },
+      ['https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600'
+      ], 5, 3, 1, 2
     ),
+    new List(
+      'Beirut Central District', new Date(), 230, 4.5,
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      { lat: 33.9055, lng: 35.5069 },
+      ['https://images.pexels.com/photos/136739/pexels-photo-136739.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/996954/pexels-photo-996954.jpeg?auto=compress&cs=tinysrgb&w=800',
+      'https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=600'
+      ], 1, 3, 1, 2
+    )
   ]
 
   getList() {
