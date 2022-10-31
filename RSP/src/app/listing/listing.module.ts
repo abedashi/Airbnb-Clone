7c9b872fb62bf4dce3382f18d49e05044f696261
habibashi <habibashi187@gmail.com>
2022-11-01 01:10:19 +0200

    add items to list

--- RSP/src/app/listing/listing.module.ts
+++ RSP/src/app/listing/listing.module.ts
@@ -3,39 +3,42 @@ export class List {
     date: Date;
     price: number;
     rate: number;
-    image: string;
     coordinates: Coordinates;
     images: string[];
     guests: number;
     bedrooms: number;
     beds: number;
     bath: number;
+    address: string;
+    apartmentName: string;
 
 
     constructor(name: string,
         date: Date,
         price: number,
         rate: number,
-        image: string,
         coordinates: Coordinates,
         images: string[],
         guests: number,
         bedrooms: number,
         beds: number,
         bath: number,
+        address: string,
+        apartmentName: string
 
     ) {
         this.name = name;
         this.date = date;
         this.price = price;
         this.rate = rate;
-        this.image = image;
         this.coordinates = coordinates;
         this.images = images;
         this.guests = guests;
         this.bedrooms = bedrooms;
         this.beds = beds;
         this.bath = bath;
+        this.address = address;
+        this.apartmentName = apartmentName;
     }
 }
 
