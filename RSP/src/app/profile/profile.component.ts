9a83ebf352685d0922a5d2a48f456e1429504881
habibashi <habibashi187@gmail.com>
2022-11-06 16:33:18 +0200

    router outlet

--- RSP/src/app/profile/profile.component.ts
+++ RSP/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  editProfile: boolean = false;
+  editProfileForm: boolean = true;
+  id: number = 1;
 
-  constructor() { }
+  edit: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+  onclick() {
+    // if (this.editProfile == false) {
+    //   this.editProfile = true;
+    //   this.editProfileForm = false;
+    // }else {
+    //   this.editProfile = false;
+    //   this.editProfileForm = true;
+    // }
+    if (!this.edit) {
+      this.router.navigate([this.id, '/edit'], { relativeTo: this.route });
+      this.edit = true;
+    } else {
+      this.router.navigate([this.id], { relativeTo: this.route });
+      this.edit = false;
+    }
   }
 
 }
