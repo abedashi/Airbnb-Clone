08b4fff194cb171c74b6d2cdf19cae3396a0e8de
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-11-16 00:38:13 +0200

    delete unused component

--- RSP/src/app/app.module.ts
+++ RSP/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,10 +11,6 @@ import { ListingComponent } from './listing/listing.component';
 import { MapsComponent } from './maps/maps.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AddListComponent } from './add-list/add-list.component';
-import { Form1Component } from './add-list/form1/form1.component';
-import { Form2Component } from './add-list/form2/form2.component';
-import { Form3Component } from './add-list/form3/form3.component';
-import { Form4Component } from './add-list/form4/form4.component';
 import { ListComponent } from './listing/list/list.component';
 
 @NgModule({
@@ -26,15 +22,12 @@ import { ListComponent } from './listing/list/list.component';
     ListingViewComponent,
     PageNotFoundComponent,
     AddListComponent,
-    Form1Component,
-    Form2Component,
-    Form3Component,
-    Form4Component,
     ListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     GoogleMapsModule,
     AppRoutingModule,
   ],
