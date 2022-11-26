ef1f9c3a8ba145ea21925a5b5c97117e2edc4684
habibashi <habibashi187@gmail.com>
2022-11-26 21:06:58 +0200

    subscribe form get and updating values

--- RSP/src/app/profile/profile.component.ts
+++ RSP/src/app/profile/profile.component.ts
@@ -1,32 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { PostProfileService } from './post-profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  editProfile: boolean = false;
-  editProfileForm: boolean = true;
-  id: number = 1;
-
-  edit: boolean = false;
-  constructor() { }
+export class ProfileComponent implements OnInit, OnDestroy {
+  view: boolean = false;
+  sub: Subscription
+  res: {};
+  constructor(
+    private profileService: PostProfileService
+  ) { }
 
   ngOnInit(): void {
+    this.sub = this.profileService.getProfileData()
+      .subscribe(resData => {
+        this.res = resData;
+        console.log(this.res);
+    });
   }
-  onclick() {
-    if (this.editProfile == false) {
-      this.editProfile = true;
-      this.editProfileForm = false;
-    }else {
-      this.editProfile = false;
-      this.editProfileForm = true;
-    }
+  changeImg(form: NgForm) {
+    console.log(form.value.url);
+    const image = form.value.url;
+    this.profileService.updateImage(image).subscribe(resData => {
+      this.profileService.getProfileData()
+      .subscribe(resData => {
+        this.res = resData;
+        console.log(this.res);
+      })
+    });
+    form.reset();
   }
-  back(event: boolean) {
-    this.editProfileForm = event;
-    this.editProfile = false;
+  
+  onSubmit(form: NgForm){
+    console.log(form.value.about)
+    const about = form.value.about;
+    const job = form.value.job;
+    const location = form.value.location;
+    const language = form.value.language;
+    this.sub = this.profileService.updateProfile(about, language, location, job)
+      .subscribe(resData => {
+        this.profileService.getProfileData()
+          .subscribe(resData => {
+          this.res = resData;
+          console.log(this.res);
+      });
+      }
+    );
+    this.view = !this.view;
+  }
+
+  onClick() {
+    this.view = !this.view;
+  }
+
+  onBack() {
+    this.view = !this.view;
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
