018146a2c1d559f8a7554d0e01045532f903a1fa
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-11-26 19:21:58 +0200

    get all appartments from database

--- RSP/src/app/listing/list/list.component.ts
+++ RSP/src/app/listing/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ServiceService } from 'src/app/service.service';
 import { List } from '../listing.module';
 
@@ -8,18 +9,24 @@ import { List } from '../listing.module';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
-  listing!: List[];
+export class ListComponent implements OnInit, OnDestroy {
+  listing: Subscription;
+  res: List[];
   constructor(private service: ServiceService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listing = this.service.getList();
-    console.log(this.listing);
+    this.listing = this.service.getData().subscribe(resData => {
+      this.res = resData;
+      console.log(resData);
+    });
   }
   onClick() {
     this.router.navigate(['/maps']);
   }
   onListingView(i: number) {
-    this.router.navigate([i], { relativeTo: this.route});
+    this.router.navigate([i], { relativeTo: this.route });
+  }
+  ngOnDestroy(): void {
+    this.listing.unsubscribe();
   }
 }
