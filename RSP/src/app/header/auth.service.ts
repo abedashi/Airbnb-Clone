863720f2d3cbd2b10ef7ea22c072be657466876a
habibashi <habibashi187@gmail.com>
2022-11-28 20:20:49 +0200

    auto login and auto login and expiration time

--- RSP/src/app/header/auth.service.ts
+++ RSP/src/app/header/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
+import { BehaviorSubject, tap, throwError } from 'rxjs';
 import { User } from './user.model';
 
 export interface AuthToken {
@@ -17,6 +17,7 @@ export interface AuthToken {
 
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(
     private http: HttpClient,
@@ -53,9 +54,40 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      id: string;
+      username: string;
+      _token: string;
+      _exp: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData){
+      return;
+    }
+    const loadedUser = new User(userData.id, userData.username, userData._token, new Date(userData._exp));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._exp).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(exp: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, exp);
+  }
+
   logout() {
     this.user.next(null);
+    localStorage.removeItem("userData");
     this.router.navigate(['/listing']);
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
   }
 
   private handleAuthentication(id: string, username: string, token: string, exp: number) {
@@ -67,6 +99,8 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    this.autoLogout(exp * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
