717b2373e3169729b9846c93809f15c37a2a58ee
habibashi <habibashi187@gmail.com>
2022-11-28 20:22:30 +0200

    watchlist class

---
+++ RSP/src/app/watsh-list/watchlist.module.ts
@@ -0,0 +1,17 @@
+export class Watchlist {
+    id?: number;
+    appartment_id: number;
+    userId: number;
+    appName: string;
+    images: string[];
+    created_at: Date;
+    price: number;
+  constructor(id: number,appartment_id: number, userId: number, appName: string, images: string[], price: number) {
+    this.id = id;
+    this.appartment_id = appartment_id;
+    this.userId = userId;
+    this.appName = appName;
+    this.images = images;
+    this.price = price;
+  }
+}
--- RSP/src/app/watsh-list/watsh-list.component.ts
+++ RSP/src/app/watsh-list/watsh-list.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { List } from '../listing/listing.module';
+import { Subscription } from 'rxjs';
 import { ServiceService } from '../service.service';
+import { Watchlist } from './watchlist.module';
 
 @Component({
   selector: 'app-watsh-list',
   templateUrl: './watsh-list.component.html',
-  styleUrls: ['./watsh-list.component.css']
+  styleUrls: ['./watsh-list.component.css'],
 })
-export class WatshListComponent implements OnInit {
-  watchLists!: List[];
-  constructor(private service: ServiceService, private router: Router, private route: ActivatedRoute) { }
+export class WatshListComponent implements OnInit, OnDestroy {
+  watchlist: Subscription;
+  watchLists: Watchlist[];
+  constructor(
+    private service: ServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.watchLists = this.service.getWatchlist();
+    // this.watchLists = this.service.getWatchlist();
+    this.watchlist = this.service.getWatchlistData().subscribe((resData) => {
+      this.watchLists = resData;
+      console.log(resData);
+    });
   }
   onListingView(i: number) {
-    this.router.navigate(['/listing/', i]);
+    this.router.navigate([i], { relativeTo: this.route });
+  }
+  ngOnDestroy(): void {
+    this.watchlist.unsubscribe();
   }
 }
