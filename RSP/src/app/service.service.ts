2d1935f13ec737ffa78be0247185d2b08940452e
habibashi <habibashi187@gmail.com>
2022-11-28 20:08:56 +0200

    create watchlist data and get data from api

--- RSP/src/app/service.service.ts
+++ RSP/src/app/service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { List } from './listing/listing.module';
+import { Watchlist } from './watsh-list/watchlist.module';
 
 @Injectable({
   providedIn: 'root'
@@ -10,34 +11,45 @@ export class ServiceService {
     private http: HttpClient
   ) { }
 
-  listing: List[] = [
-    
-  ]
-  addList(newList: List) {
-    this.listing.push(newList);
-  }
+  // listing: List[] = []
+  // addList(newList: List) {
+  //   this.listing.push(newList);
+  // }
   // arr: List[] = [];
+  
+  createWachlistData(index: number) {
+    this.http
+      .get('http://localhost/Airbnb-Clone-API/api/watchlist/create.php', {
+        params: new HttpParams().set('appartment_id', index)
+      }).subscribe();
+  }
+
+  getWatchlistData() {
+    return this.http
+      .get<Watchlist[]>('http://localhost/Airbnb-Clone-API/api/watchlist/get.php')
+  }
+
   getData() {
     return this.http
       .get<List[]>('http://localhost/Airbnb-Clone-API/api/appartments-list/get.php')
   }
 
-  watchList: List[] = [];
+  // watchList: List[] = [];
 
-  getWatchlist() {
-    return this.watchList;
-  }
-  addToWatchListId(index: number) {
-    this.watchList.push(this.listing[index]);
-  }
+  // getWatchlist() {
+  //   return this.watchList;
+  // }
+  // addToWatchListId(index: number) {
+  //   this.watchList.push(this.listing[index]);
+  // }
 
-  getList() {
-    return this.listing;
-  }
+  // getList() {
+  //   return this.listing;
+  // }
 
-  getListId(index: number) {
-    return this.listing[index];
-  }
+  // getListId(index: number) {
+  //   return this.listing[index];
+  // }
 
   getSingleData(index: number){
     return this.http
